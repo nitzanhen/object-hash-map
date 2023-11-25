a2b65a1bb44dfe1b77fc6f455ef694d007b668bf
Nitzan Hen <code@nitzanhen.net>
2023-11-25 14:44:10 +0200

    Export everything through `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { hash } from './hash';
-
-export const x = 4;
-
-console.log(hash({ x: 3 }));
-console.log(hash({ x: 4 }));
-
-class Foo {
-  constructor(public x: number) {}
-}
-
-console.log(hash(new Foo(3)));
-
-
-
+export * from './ObjectMap';
+export * from './equals';
+export * from './hash';
